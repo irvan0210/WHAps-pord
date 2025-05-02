eca67c8a64634df1c3acca75cc13892eba02c060
Nanang221 <nanang.andriani@whitehorse.co.id>
2025-05-02 15:52:35 +0700

    Master Training List

---
+++ MateriTrainingListU.dfm
@@ -0,0 +1,99 @@
+object MateriTrainingList: TMateriTrainingList
+  Left = 364
+  Top = 209
+  BorderIcons = []
+  BorderStyle = bsNone
+  Caption = 'Materi Training List'
+  ClientHeight = 531
+  ClientWidth = 757
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'Tahoma'
+  Font.Style = []
+  FormStyle = fsMDIChild
+  OldCreateOrder = False
+  Position = poDesktopCenter
+  Visible = True
+  OnClose = FormClose
+  OnShow = FormShow
+  PixelsPerInch = 96
+  TextHeight = 13
+  object Label1: TLabel
+    Left = 504
+    Top = 12
+    Width = 19
+    Height = 13
+    Caption = 'Cari'
+  end
+  object StrGrid: TZColorStringGrid
+    Left = 2
+    Top = 40
+    Width = 751
+    Height = 441
+    ColCount = 9
+    DefaultRowHeight = 22
+    DefaultDrawing = False
+    FixedColor = clSkyBlue
+    FixedCols = 0
+    RowCount = 2
+    Options = [goFixedVertLine, goFixedHorzLine, goVertLine, goHorzLine]
+    TabOrder = 0
+    OnDblClick = StrGridDblClick
+    OnSelectCell = StrGridSelectCell
+    DefaultCellStyle.Font.Charset = DEFAULT_CHARSET
+    DefaultCellStyle.Font.Color = clWindowText
+    DefaultCellStyle.Font.Height = -11
+    DefaultCellStyle.Font.Name = 'MS Sans Serif'
+    DefaultCellStyle.Font.Style = []
+    DefaultCellStyle.BGColor = clWindow
+    DefaultFixedCellStyle.Font.Charset = DEFAULT_CHARSET
+    DefaultFixedCellStyle.Font.Color = clWindowText
+    DefaultFixedCellStyle.Font.Height = -11
+    DefaultFixedCellStyle.Font.Name = 'MS Sans Serif'
+    DefaultFixedCellStyle.Font.Style = []
+    DefaultFixedCellStyle.BGColor = clSkyBlue
+    LineDesign.LineUpColor = clWhite
+  end
+  object Selesai: TButton
+    Left = 381
+    Top = 496
+    Width = 75
+    Height = 25
+    Caption = 'Selesai'
+    TabOrder = 1
+    OnClick = SelesaiClick
+  end
+  object GroupTotal: TGroupBox
+    Left = 5
+    Top = 485
+    Width = 92
+    Height = 33
+    Enabled = False
+    TabOrder = 2
+    object Label2: TLabel
+      Left = 8
+      Top = 11
+      Width = 24
+      Height = 13
+      Caption = 'Total'
+    end
+    object Total: TMemo
+      Left = 42
+      Top = 8
+      Width = 47
+      Height = 22
+      Alignment = taRightJustify
+      TabOrder = 0
+    end
+  end
+  object Cari: TEdit
+    Left = 528
+    Top = 8
+    Width = 225
+    Height = 21
+    TabOrder = 3
+    OnChange = CariChange
+  end
+end
---
+++ MateriTrainingListU.pas
@@ -0,0 +1,207 @@
+unit MateriTrainingListU;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, StdCtrls, Grids, ZColorStringGrid, WHUnit, AdoDB;
+
+type
+  TMateriTrainingList = class(TForm)
+    StrGrid: TZColorStringGrid;
+    Selesai: TButton;
+    GroupTotal: TGroupBox;
+    Total: TMemo;
+    Cari: TEdit;
+    Label1: TLabel;
+    Label2: TLabel;
+    procedure FormClose(Sender: TObject; var Action: TCloseAction);
+    procedure SelesaiClick(Sender: TObject);
+    procedure FormShow(Sender: TObject);
+    procedure CariChange(Sender: TObject);
+    procedure StrGridDblClick(Sender: TObject);
+    procedure StrGridSelectCell(Sender: TObject; ACol, ARow: Integer;
+      var CanSelect: Boolean);
+  private
+    { Private declarations }
+    MateriArr:Array of TArrString2;
+    FormRequest:String;
+    IntCol,IntRow:Integer;
+    procedure RefreshData;
+    procedure RefreshGrid;
+    procedure InitGrid;
+    procedure Search;
+  public
+    { Public declarations }
+    constructor Create(AOwner:TComponent;Form_Request:String='');Overload;
+  end;
+
+var
+  MateriTrainingList: TMateriTrainingList;
+
+implementation
+
+uses MainU, EmployeeHistoryTrainingFormU, EmployeeHistoryTrainingRptU;
+
+{$R *.dfm}
+
+constructor TMateriTrainingList.Create(AOwner:TComponent;Form_Request:String='');
+begin
+  FormRequest:=Form_Request;
+  Main.WriteLog('Form Open: ListParts='+Form_Request);
+  Inherited Create(AOwner);
+end;
+
+procedure TMateriTrainingList.InitGrid;
+var IntCount:Integer;
+begin
+  StrGrid.ColCount:=2;
+  StrGrid.RowCount:=1;
+  StrGrid.WordWrap:=True;
+  StrGrid.ColWidths[0]:=700;
+  StrGrid.ColWidths[1]:=0;
+
+  StrGrid.Cells[0,0]:='Materi';
+
+  for IntCount:=0 to StrGrid.ColCount-1 do
+  begin
+    StrGrid.CellStyle[IntCount,0].HorizontalAlignment:=taCenter;
+  end;
+
+  for IntCount:=0 to StrGrid.ColCount-1 do
+    StrGrid.Cells[IntCount,2]:='';
+end;
+
+procedure TMateriTrainingList.RefreshData;
+var Qry:TADOQuery;
+    StrQry,StrReq:String;
+    IntCount:Integer;
+begin
+
+  Qry:=TADOQuery.Create(Self);
+  Qry.Connection:=Main.MyConnection;
+  Qry.CommandTimeout := 3600;
+  Main.M_Busy;
+
+  if Main.OpenDb then begin
+    SetLength(MateriArr,0);
+
+    StrQry:='SELECT * from wh_materi_training where status=1 order by materi ASC;';
+    Main.WriteLog('SQL :'+StrQry,2);
+    Qry.SQL.Clear;
+    Qry.SQL.Add(StrQry);
+    Qry.Open;
+    IntCount:=0;
+    if Qry.RecordCount>0 then while not(Qry.Eof) do begin
+      SetLength(MateriArr,IntCount+1);
+      MateriArr[IntCount][0]:= Qry.FieldValues['materi_training_id'] ;
+      MateriArr[IntCount][1]:= Qry.FieldValues['materi'] ;
+      Inc(IntCount);
+      Qry.Next;
+    end;
+    Qry.Close;
+  end;
+
+  FreeAndNil(Qry);
+  Main.CloseDb;
+  Main.M_Normal;
+end;
+
+procedure TMateriTrainingList.RefreshGrid;
+var IntCount,IntTotal,lengt:Integer;
+begin
+  if Length(MateriArr)>0 then StrGrid.RowCount:=Length(MateriArr)+1
+  else begin
+    StrGrid.RowCount:=1;
+  end;
+
+  IntTotal:=0;
+  lengt:= Length(MateriArr)-1;
+  for IntCount:=0 to Length(MateriArr)-1 do begin
+    Application.ProcessMessages;
+    StrGrid.Cells[0,IntCount+1]:=MateriArr[IntCount][1];
+    StrGrid.Cells[1,IntCount+1]:=MateriArr[IntCount][0];
+    StrGrid.CellStyle[0,IntCount+1].HorizontalAlignment:=taLeftJustify;
+    Inc(IntTotal);
+  end;
+  Total.Text:=IntToStr(IntTotal);
+end;
+
+
+procedure TMateriTrainingList.FormClose(Sender: TObject;
+  var Action: TCloseAction);
+begin
+  Action:=caFree;
+end;
+
+procedure TMateriTrainingList.SelesaiClick(Sender: TObject);
+begin
+  Close;
+end;
+
+procedure TMateriTrainingList.FormShow(Sender: TObject);
+begin
+  Cari.Text:='';
+  InitGrid;
+  RefreshData;
+  RefreshGrid;
+  Cari.SetFocus;
+end;
+
+procedure TMateriTrainingList.Search;
+var Count,Count2,Count3,Count4,IntCount5,IntCount2:Integer;
+    IsTrue:Boolean;
+begin
+  if Trim(Cari.Text)<>'' then begin
+    Main.M_Busy;
+    InitGrid;
+    Count2:=1;
+    for Count:=0 to Length(MateriArr)-1 do begin
+      IsTrue:=False;
+      for Count3:=0 to 1 do
+      if (StrPos(PChar(UpperCase(MateriArr[Count][Count3])),PChar(UpperCase(Cari.Text)))<>nil) then IsTrue:=True;
+      if IsTrue then begin
+        StrGrid.RowCount:=Count2+1;
+        StrGrid.Cells[0,Count2]:=MateriArr[Count][1];
+        StrGrid.Cells[1,Count2]:=MateriArr[Count][0];
+        for IntCount2:=0 to StrGrid.ColCount-1 do StrGrid.CellStyle[IntCount2,Count2].Font.Color:=clWindowText;
+        Inc(Count2);
+      end;
+    end;
+    Main.M_Normal;
+  end;
+end;
+
+procedure TMateriTrainingList.CariChange(Sender: TObject);
+begin
+  if Trim(Cari.Text)='' then begin
+    RefreshData;
+    RefreshGrid;
+  end else
+  begin
+    Search;
+  end;
+end;
+
+procedure TMateriTrainingList.StrGridDblClick(Sender: TObject);
+begin
+  if (IntRow>0) then
+  begin
+    if FormRequest='EMPLOYETRAINING' then begin
+      EmployeeHistoryTrainingForm.Materi.Text:=StrGrid.Cells[0,IntRow];
+      MateriTrainingID:=StrGrid.Cells[1,IntRow];
+    end else if FormRequest='EMPLOYETRAININGRPT' then begin
+      EmplHistoryTrainingRpt.Materi.Text:=StrGrid.Cells[0,IntRow];
+    end;
+    Close;
+  end;
+end;
+
+procedure TMateriTrainingList.StrGridSelectCell(Sender: TObject; ACol,
+  ARow: Integer; var CanSelect: Boolean);
+begin
+  IntCol:=ACol;
+  IntRow:=ARow;
+end;
+
+end.
